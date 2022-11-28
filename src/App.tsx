e4a7adad5c875f67a82c13deca60e0a179d58e6f
Tùng Dương <tungduong.kaka64@gmail.com>
2022-11-28 17:31:05 +0700

    Bai 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState<any>("");
+  const [newText, setnewText] = useState();
+
+  const onSubmit = () => {
+    setText((prev: any) => {
+      const camelize = (text: any) => {
+        const a = text
+          .toLowerCase()
+          .replace(/[-_\s.]+(.)?/g, (word: any, index: any) =>
+            index ? index.toUpperCase() : ""
+          );
+        return a.substring(0, 1).toLowerCase() + a.substring(1);
+      };
+      setnewText(camelize(text));
+      return newText;
+    });
+    setText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e: any) => setText(e.target.value)}
+      />
+      <button onClick={onSubmit}>Submit</button>
+      <p>Chuỗi trả về: {newText} </p>
     </div>
   );
 }
