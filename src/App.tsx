0ebfc3cb636f19095f2c663c52c278715f7cd0aa
Tùng Dương <tungduong.kaka64@gmail.com>
2022-11-29 14:37:29 +0700

    Bai10-Giaithua

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,36 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState<any[]>([]);
-  for (let index = 10; index <= 200; index++) {
-    if (index % 7 == 0 && index % 5 != 0) {
-      number.push(index);
+  const [number, setNumber] = useState<any>();
+  const [factorial, setFactorial] = useState<any>();
+  const [expression, setExpression] = useState<any>("");
+  const handleClick = () => {
+    let check = 1;
+    let expression = "";
+    for (let i = 1; i <= number; i++) {
+      check *= i;
+      if (i < number) {
+        expression = expression + i + "*";
+      } else {
+        expression = expression + i + "=";
+      }
     }
-  }
+    setExpression(expression);
+    setFactorial(check);
+  };
 
   return (
     <div className="container">
-      <p>{number.join(", ")}</p>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={handleClick}>Tính</button>
+
+      <p>
+        Giai thừa của {`${number || ""}`} là: {expression}{factorial}
+      </p>
     </div>
   );
 }
