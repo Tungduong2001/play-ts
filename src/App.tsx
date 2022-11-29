838ec7080aebed19295a60e94e02af5f73d7585c
Tùng Dương <tungduong.kaka64@gmail.com>
2022-11-29 10:50:11 +0700

    Bai-6

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const checked = (text: string, a: string, b?: number) => {
-    if (text.endsWith(a) || text.endsWith(a, b)) {
-      return true;
-    } else {
-      return false;
+  const [hours, setHours] = useState<any[]>([]);
+  for (let index = 1; index <= 24; index++) {
+    for (let j = 0; j <= 50; j += 10) {
+      hours.push(index + ":" + j);
     }
-  };
-  console.log(checked("abcd", "b", 4));
-  console.log(checked("abcd", "d"));
+  }
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <p>chọn giờ</p>
+      <select name="" id="">
+        {hours.map((item: any) => (
+          <option key={item} value="">
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default App;
